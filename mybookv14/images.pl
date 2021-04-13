88b7a75b8c1b94eb0fcc1acb6c3c8e9aa77aa429
skeggsy42 <phil@skeggs.net>
2021-04-13 17:01:25 +0100

    Add files via upload

---
+++ mybookv14/images.pl
@@ -0,0 +1,44 @@
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# Associate images original text with physical files.
+
+
+$key = q/displaystyleFV=PV(1+i)n^2;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.81ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img1.svg"
+ ALT="$\displaystyle FV=PV(1+i)n^2$">|; 
+
+$key = q/displaystylePV=100(1+0.03)^10=dollar74.41;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.81ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img6.svg"
+ ALT="$\displaystyle PV=100(1+0.03)^10 = \$74.41$">|; 
+
+$key = q/displaystylePV=FV(1+i)^n;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img5.svg"
+ ALT="$\displaystyle PV=FV(1+i)^n$">|; 
+
+$key = q/displaystylei=10sqrt{frac{100}{P60-1}}=5.2%;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 5.88ex; vertical-align: -2.04ex; " SRC="|."$dir".q|img4.svg"
+ ALT="$\displaystyle i=10 \sqrt{\frac{100}{P60-1}}=5.2\%$">|; 
+
+$key = q/displaystylei=nsqrt{frac{FV}{PV-1}};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 5.88ex; vertical-align: -2.00ex; " SRC="|."$dir".q|img3.svg"
+ ALT="$\displaystyle i=n \sqrt{\frac{FV}{PV-1}}$">|; 
+
+$key = q/displaystyleleft(frac{1}{sqrt{x}}right);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 5.79ex; vertical-align: -2.32ex; " SRC="|."$dir".q|img2.svg"
+ ALT="$\displaystyle \left(\frac{1}{\sqrt{x}}\right)$">|; 
+
+$key = q/{tcolorbox}ROE=(earnings+interestexpenseondebt)slash(equity+debt)*100.{tcolorbox};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img8.svg"
+ ALT="\begin{tcolorbox}
+ROE = (earnings+interest expense on debt)/(equity+debt) *100.
+\end{tcolorbox}">|; 
+
+$key = q/{tcolorbox}ROE=Netincomeslashbookvalueofequity{tcolorbox};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img7.svg"
+ ALT="\begin{tcolorbox}
+ROE = Net income/book value of equity
+\end{tcolorbox}">|; 
+
+1;
+
