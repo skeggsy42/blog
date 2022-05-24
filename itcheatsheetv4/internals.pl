c11b56e08c8fd68558beea55b2afd99913251bda
Phil Skeggs <phil@skeggs.net>
2022-05-24 20:15:52 +0100

    modified:   index.html 	new file:   itcheatsheetv4/contents.png 	new file:   itcheatsheetv4/index.html 	new file:   itcheatsheetv4/internals.pl 	new file:   itcheatsheetv4/itcheatsheetv4.css 	new file:   itcheatsheetv4/labels.pl 	new file:   itcheatsheetv4/next.png 	new file:   itcheatsheetv4/next_g.png 	new file:   itcheatsheetv4/node1.html 	new file:   itcheatsheetv4/node10.html 	new file:   itcheatsheetv4/node11.html 	new file:   itcheatsheetv4/node12.html 	new file:   itcheatsheetv4/node13.html

---
+++ itcheatsheetv4/internals.pl
@@ -0,0 +1,10 @@
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_Hacking:_The_Art_of_Exploitation__2nd_Ed./;
+$ref_files{$key} = "$dir".q|node36.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ itcheatsheetv4/labels.pl
@@ -0,0 +1,17 @@
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_Hacking:_The_Art_of_Exploitation__2nd_Ed./;
+$external_labels{$key} = "$URL/" . q|node36.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# labels from external_latex_labels array.
+
+
+1;
+
